abe19baf78de9aa4504603cf397dc0a771eadcfb
Saipranav Koyyada <ksaipranav@gmail.com>
2020-03-20 11:30:49 +0100

    Update mailer_module.py

--- SPI_model_S/mailer_module.py
+++ SPI_model_S/mailer_module.py
@@ -1,7 +1,7 @@
 EMAIL_USE_TLS=True
 EMAIL_HOST='smtp.gmail.com'
-EMAIL_HOST_USER='your email'
-EMAIL_HOST_PASSWORD='add your password'
+EMAIL_HOST_USER='yourname@email.com'
+EMAIL_HOST_PASSWORD='password'
 EMAIL_PORT=587
-DEFAULT_FROM_EMAIL = 'your email'
-DEFAULT_TO_EMAIL = 'your email'
+DEFAULT_FROM_EMAIL = 'yourname@email.com'
+DEFAULT_TO_EMAIL = 'yourname@email.com'
